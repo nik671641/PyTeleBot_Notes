950b44dfc76e07de44980f2f2fd70acaf6f23b15
Narzuullo <117679286+nik671641@users.noreply.github.com>
2024-01-06 11:45:40 +0200

    Add files via upload

--- Memo_Rover_Bot/main.py
+++ Memo_Rover_Bot/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command, CommandObject
 from aiogram.types import Message
 
@@ -14,28 +14,29 @@ dp = Dispatcher()
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
 @dp.message(Command("start"))
 async def cmd_add(message: Message):
-    await message.answer(f"Hello {message.from_user.first_name}")
-
+    await message.answer(f"Hello {message.from_user.first_name} \n Привет я бот для заметок используй команды \n /add - добавление заметки \n /getall - получение всех заметок")
 
 
 @dp.message(Command("add"))
-async def cmd_add(message: types.Message, command: CommandObject):
-    commands = command.args
-    if commands:
-        with SessionLocal() as db:
-            new_note = Note(text=commands)
-            db.add(new_note)
-            db.commit()
-            print(commands)
-    else:
-        await message.answer(f"Заметка не добавлена")
+async def cmd_add(message: Message):
+    commands = message.text.split('/add')[1:]
+    for command_text in commands:
+        note_text = command_text.strip()
+        if note_text:
+            with SessionLocal() as db:
+                new_note = Note(text=note_text)
+                db.add(new_note)
+                db.commit()
+                print(f"Заметка добавлена: {note_text}")
+        else:
+            await message.answer(f"Заметка не добавлена")
 
 
 @dp.message(Command("getall"))
 async def get_all_notes(message: types.Message):
-
     with SessionLocal() as db:
         notes = db.query(Note).all()
 
@@ -45,9 +46,11 @@ async def get_all_notes(message: types.Message):
     else:
         await message.answer("У вас нет заметок.")
 
+
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
